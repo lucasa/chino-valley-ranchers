4a40e8df2ca7ef53bf41feceb86a9e4ddfd0fdd2
Mike Schlosser <ms122r@gmail.com>
2021-05-06 12:42:10 -0300

    updates

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { usePlugin } from 'tinacms'
+import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 
 import { Nav } from '../../components/Nav'
 import { Hero } from '../../components/hero/Hero'
@@ -7,7 +9,22 @@ import { HeadingPaperEdge } from '../../components/heading/HeadingPaperEdge'
 
 export default function Products({ file }) {
 
-  const data = file.data
+  //const data = file.data
+  const formOptions = {
+    label: 'Products Page',
+    fields: [
+      {
+        name: 'heading',
+        component: 'text'
+      },
+    ],
+  }
+
+  // Registers a JSON Tina Form
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
+
+  useGithubToolbarPlugins()
 
   return (
 
