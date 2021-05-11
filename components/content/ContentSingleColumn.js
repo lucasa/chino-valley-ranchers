873cda9be21062512e1e83286ac1f7d62d6291c3
Mike Schlosser <ms122r@gmail.com>
2021-05-11 12:56:19 -0300

    single column clock

--- components/content/ContentSingleColumn.js
+++ components/content/ContentSingleColumn.js
@@ -12,14 +12,6 @@ export function ContentSingleColumn({data, index}) {
         <div className="relative pt-8 -top-36 pb-12 bg-no-repeat bg-cover" style={{ backgroundImage: `url('/images/blue-bg.png')` }}>
             <div className="max-w-5xl mx-auto pt-8 lg:pt-32 text-center">
                 <InlineBlocks name="blocks" blocks={CONTENT_SINGLE_COLUMN_BLOCKS} />
-
-                {content.content.map((block, index) => (
-                    <div key={index} className="mb-12 lg:mb-24 px-8 xl:px-0">
-                        <h4 className="font-ultra uppercase text-chinoyellow text-3xl lg:text-6xl tracking-wide mb-4">{block.heading}</h4>
-                        <p className="text-xl lg:text-3xl text-white tracking-wide">{block.subheading}</p>
-                    </div>
-                ))}
-
             </div>
         </div>
 
