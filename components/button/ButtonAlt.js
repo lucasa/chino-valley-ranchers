87e3234f4f46dabcbe20cbe4218ca7fe810bbd84
Mike Schlosser <ms122r@gmail.com>
2021-05-11 14:49:43 -0300

    single column alt

--- components/button/ButtonAlt.js
+++ components/button/ButtonAlt.js
@@ -2,13 +2,15 @@ import { InlineText, InlineTextarea, InlineImage, BlocksControls, InlineGroup } 
 
 export function ButtonAlt(props) {
 
+    console.log(props)
+
     return (
         <div className="relative -top-48 pb-4 lg:pb-12 bg-repeat-y bg-cover" style={{ backgroundImage: `url('/images/bg-paper.png')` }}>
             <div className="max-w-5xl mx-auto text-center">
                 <div className="mb-2 px-8 xl:px-0">
                     <BlocksControls index={props.index}>
                         <div className="text-center py-8 lg:py-16">
-                            <a href={props.data.button.link.url} className="px-5 py-2 bg-chinored text-white uppercase font-din font-bold text-xl lg:text-4xl rounded-lg tracking-wide">{props.data.button.text}</a>
+                            <a href="#" className="px-5 py-2 bg-chinored text-white uppercase font-din font-bold text-xl lg:text-4xl rounded-lg tracking-wide">Find Store</a>
                         </div>
                     </BlocksControls>
                 </div>
