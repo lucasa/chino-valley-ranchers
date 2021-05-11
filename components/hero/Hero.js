8fc4f41d572bde90bb84efcee8841f47b67b79ff
Mike Schlosser <ms122r@gmail.com>
2021-05-11 11:30:16 -0300

    test

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -15,7 +15,7 @@ export function Hero(props) {
                     parse={(media) => {
                         return `/${media.id}`;
                     }}
-                    uploadDir={() => "/images/"}
+                    uploadDir={() => "images/"}
                     alt="Organic Eggs"
                 />
                 {/*<Image
