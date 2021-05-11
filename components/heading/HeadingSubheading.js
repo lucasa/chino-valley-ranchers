26fabeaf135e9063008caea1df71bc321722aabe
Mike Schlosser <ms122r@gmail.com>
2021-05-11 13:00:19 -0300

    textarea

--- components/heading/HeadingSubheading.js
+++ components/heading/HeadingSubheading.js
@@ -6,7 +6,7 @@ export function HeadingSubheading(props) {
         <BlocksControls index={props.index}>
             <div className="mb-12 lg:mb-24 px-8 xl:px-0">
                 <h4 className="font-ultra uppercase text-chinoyellow text-3xl lg:text-6xl tracking-wide mb-4"><InlineText name="heading" /></h4>
-                <p className="text-xl lg:text-3xl text-white tracking-wide"><InlineTextArea name="subheading" /></p>
+                <p className="text-xl lg:text-3xl text-white tracking-wide"><InlineTextarea name="subheading" /></p>
             </div>
         </BlocksControls>
     )
